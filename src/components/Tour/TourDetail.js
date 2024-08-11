697ad508b7f712f16988abd32c3a5b956f3c18b1
demo <abdullah.qureshi@emumba.com>
2024-08-11 22:08:55 +0500

    featues added

--- src/components/Tour/TourDetail.js
+++ src/components/Tour/TourDetail.js
@@ -11,24 +11,23 @@ function TourDetail() {
 
   return (
     <>
-      <div>
-        <div>
+      <div className='page'>
+        <div className='nav' >
           <Navbar />
         </div>
         {name && para && (
           <div className="sec-1">
             <h1>{name}</h1>
             {image && image.length > 0 && (
-              <div>
+              <div className='pic'>
                 {image.map((image, index) => {
-                  // Create a class name based on the index
                   const className = `image image${index + 1}`
                   return (
                     <img
                       key={index}
                       src={image}
                       alt={`Profile ${index + 1}`}
-                      className={className} // Apply the class name
+                      className={className} 
                     />
                   )
                 })}
@@ -45,25 +44,79 @@ function TourDetail() {
                 <h1>Departure Location</h1>
                 <h2>2000 Brush St, Detroit, MI 48226, United States</h2>
               </div>
-                <div className="el3">
+              <div className="el3">
                 <h1>Return</h1>
                 <h2>7:00PM on Day 3</h2>
               </div>
-                <div className="el4">
+              <div className="el4">
                 <h1>Return</h1>
-                <img src={tick} alt="" />
-                <h2>d</h2>
-                <img src={tick} alt="" />
-                <h2>g</h2>
-                <img src={tick} alt="" />
-                <h2>k</h2>
-                <img src={tick} alt="" />
-                <h2>g</h2>
-                <img src={tick} alt="" />
-                <h2>h</h2>
-                <img src={tick} alt="" />
-                <h2>h</h2>
+                <div className="i1">
+                  <img src={tick} alt="" />
+                  <h2>Basic first aid kit</h2>
+                </div>
+                <div className="i2">
+                  <img src={tick} alt="" />
+                  <h2>Comfortable Private Booked Transport</h2>
+                </div>
+                <div className="i3">
+                  <img src={tick} alt="" />
+                  <h2>Fuel Expense</h2>
+                </div>
+                <div className="i4">
+                  <img src={tick} alt="" />
+                  <h2>Mess Tent, Kitchen Utensils, and Cook</h2>
+                </div>
+                <div className="i5">
+                  <img src={tick} alt="" />
+                  <h2>Detail Guided Maps</h2>
+                </div>
+                <div className="i6">
+                  <img src={tick} alt="" />
+                  <h2>Waterproof Tents on twin/triple Sharing</h2>
+                </div>
               </div>
+            </div>
+            <div className="sec-3">
+              <h1>Itinerary Schedule</h1>
+              <div className="box">
+                <div className="box1">
+                  <div className="fl">
+                    <h1>Day 1</h1>
+                    <h2>18*C</h2>
+                  </div>
+                  <ul>
+                    <li>Tours with American Sign Language</li>
+                    <li>Audio description group tours</li>
+                    <li>Large-print gallery notes</li>
+                    <li>Launch included.</li>
+                  </ul>
+                </div>
+                <div className="box1">
+                  <div className="fl">
+                    <h1>Day 2</h1>
+                    <h2>14*C</h2>
+                  </div>
+                  <ul>
+                    <li>Tours with American Sign Language</li>
+                    <li>Audio description group tours</li>
+                    <li>Large-print gallery notes</li>
+                    <li>Launch included.</li>
+                  </ul>
+                </div>
+                <div className="box1">
+                  <div className="fl">
+                    <h1>Day 3</h1>
+                    <h2>17*C</h2>
+                  </div>
+                  <ul>
+                    <li>Tours with American Sign Language</li>
+                    <li>Audio description group tours</li>
+                    <li>Large-print gallery notes</li>
+                    <li>Launch included.</li>
+                  </ul>
+                </div>
+              </div>
+              <button><span className='btn-text'>Book Now</span></button>
             </div>
           </div>
         )}
